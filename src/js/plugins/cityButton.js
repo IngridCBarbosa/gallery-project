cb4200f28b3c0a8afa26af164e6f3b2f2c924c63
IngridCBarbosa <ingridcarolinelima@hotmail.com>
2020-08-01 15:51:04 -0300

    add filter button

---
+++ src/js/plugins/cityButton.js
@@ -0,0 +1,55 @@
+import $ from 'jquery';
+
+const duration = 600;
+
+
+function filterByCity(city) {
+    $('[wm-city]').each(function (i, e) {
+        const isTarget = $(this).attr('wm-city') === city || city == null;
+
+        if (isTarget) {
+            $(this).parent().removeClass('d-none')
+            $(this).fadeIn(duration);
+        } else {
+            $(this).fadeOut(duration, () => {
+                $(this).parent().addClass('d-none');
+            });
+        }
+
+    });
+}
+
+const cityButtons = $('[wm-city-buttons]')
+
+$.fn.cityButtons = function () {
+
+
+    const cities = new Set
+    $('[wm-city]').each(function (i, e) {
+
+        cities.add($(e).attr('wm-city'));
+    });
+
+    const buttons = Array.from(cities).map(city => {
+
+        const button = $('<button>').addClass(['btn', 'btn-info'])
+            .html(city)
+
+        button.click(e => filterByCity(city));
+        return button;
+    });
+
+    const buttonAll = $('<button>').addClass(['btn', 'btn-info', 'active'])
+        .html('Todas')
+    buttonAll.click(e => filterByCity(null));
+
+    const buttonsGroup = $('<div>').addClass(['btn-group']);
+    buttonsGroup.click(buttons);
+
+    $(this).html(buttonsGroup);
+
+    return this;
+
+}
+
+$('[wm-city-buttons]').cityButtons();
